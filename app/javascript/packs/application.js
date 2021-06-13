5a568c08705add07124c0228de2c035c6553d6e3
mahi <mohiddinoshim@yahoo.com>
2021-06-14 01:02:32 +0600

    fontawesome without gem by yarn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 // import 'packs/bootstrap'
 
+import "@fortawesome/fontawesome-free/js/all";
 // import 'bootstrap/dist/js/bootstrap'
 // import 'bootstrap/dist/css/bootstrap'
 // require("stylesheets/application.scss")
