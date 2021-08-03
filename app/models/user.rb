5b03b6da0dcc61204cedac32f238a52c487d12a9
mahi <mohiddinoshim@yahoo.com>
2021-08-03 23:05:02 +0600

    scaffold Enrollments basic validation and design

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
 
   rolify
 
+  has_many :courses
+  has_many :enrollments
+
   def to_s
     email
   end
@@ -15,7 +18,7 @@ class User < ApplicationRecord
       self.email.split(/@/).first
   end
 
-  has_many :courses
+
 
   extend FriendlyId
   friendly_id :email, use: :slugged
