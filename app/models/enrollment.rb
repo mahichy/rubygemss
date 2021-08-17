3e90912b897e16c9e18e6e6342f1c6de96d1baac
mahi <mohiddinoshim@yahoo.com>
2021-08-17 17:16:54 +0600

    gem friendly_id for enrollments

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -13,6 +13,10 @@ class Enrollment < ApplicationRecord
   validate :cant_subscribe_to_own_course
   # user cant create a subscription if course.user == current_user
   scope :panding_review, -> { where(rating: [0, nil, ""], review: [0, nil, ""]) } 
+  
+  extend FriendlyId
+  friendly_id :to_s, use: :slugged
+
   def to_s
     user.to_s + " " + course.to_s
   end
