c8573ee3befcf4e3aefa3efa0365a4b81b2cf679
mahi <mohiddinoshim@yahoo.com>
2021-10-21 17:47:22 +0600

    add counter cache across app

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,8 @@ class Course < ApplicationRecord
   validates :description,  presence: true, length: { :minimum => 5 }
   validates :title, :short_description, :language, :price, :level, presence: true
 
-  belongs_to :user
+  belongs_to :user, counter_cache: true
+  # User.find_each { |user| User.reset_counters(user.id, :courses)}
   has_many :lessons, dependent: :destroy
   has_many :enrollments
 
